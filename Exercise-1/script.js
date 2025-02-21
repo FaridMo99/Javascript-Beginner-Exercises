0c675d766daede37b6d81320c1454794ebe5ad61
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-21 20:15:59 +0100

    added restart button and function

--- Exercise-1/script.js
+++ Exercise-1/script.js
@@ -6,9 +6,9 @@ const playerAmountForm = document.querySelector(".playerAmount")
 const onePlayer = document.querySelector("#one")
 const twoPlayers = document.getElementById("two")
 const opponentNameDiv = document.querySelector(".opponentName")
-const buttonAmountOpponents = document.querySelector(".opp")
-const playerSymbol = document.querySelector(".playerSymbol")
-const opponentSymbol = document.querySelector(".opponentSymbol")
+const submitButtonAmountOpponents = document.querySelector(".opp")
+const playerSymbolDiv = document.querySelector(".playerSymbol")
+const opponentSymbolDiv = document.querySelector(".opponentSymbol")
 let opponentName = document.querySelector(".opponentNameInput")
 let opponentNameInput = document.querySelector("#opponentName")
 let playerNameInput = document.querySelector("#username")
@@ -20,11 +20,15 @@ function isFormValid(a) {
 
 closeModal.addEventListener('click', (e) => {
     e.preventDefault()
+    let playerSymbol = document.querySelector("input[name='symbol']:checked").value
+    let opponentSymbol = playerSymbol == "circle" ? "cross" : "circle"
 
     if (isFormValid(form)) {
         modal.style.display = 'none';
         playerAmountForm.classList.toggle("secondHidden")
         playerName.innerHTML = playerNameInput.value
+        playerSymbolDiv.innerHTML = `<img src="${playerSymbol}.svg" alt="${playerSymbol}">`
+        opponentSymbolDiv.innerHTML = `<img src="${opponentSymbol}.svg" alt="${opponentSymbol}">`
     } 
 
     else {
@@ -48,7 +52,7 @@ playerAmountForm.addEventListener("click", (e) => {
 }
 })
 
-buttonAmountOpponents.addEventListener("click", (e) => {
+submitButtonAmountOpponents.addEventListener("click", (e) => {
     e.preventDefault()
 
     if(onePlayer.checked) {
@@ -56,10 +60,11 @@ buttonAmountOpponents.addEventListener("click", (e) => {
         opponentName.textContent = "Computer"
     }
 
-    else if(twoPlayers.checked && opponentName !== undefined) {
+    else if(twoPlayers.checked && opponentNameInput.value != "") {
         playerAmountForm.classList.toggle("secondHidden")
         opponentName.textContent = opponentNameInput.value
     }
+
 
     else {
         alert("Player 2 has to choose a Name")
@@ -75,25 +80,61 @@ const squareArray = document.querySelectorAll(".sq")
 squareArray.forEach(element => {
 
   element.addEventListener('click', (e) => {
-    let yourSymbol = document.querySelector('input[name="symbol"]:checked').value;
+    let playerSymbol = document.querySelector("input[name='symbol']:checked").value
 
     e.target.classList.remove("hover")
-    e.target.innerHTML = `<img src="${yourSymbol}.svg" alt="${yourSymbol}">`;
+    e.target.innerHTML = `<img src="${playerSymbol}.svg" alt="${playerSymbol}">`;
 
     });
 });
 
+    //player 2 algorithm
+
+
     //computer algorithm
 
 
-//features to implement
-    //choosing how many rounds between 1/3/5
-        //scoreBoard when more than one round
-    //playing against computer
-    //reset Button
-    //popUp when game over with button wanna play again?
-    //function for 2 players or against computer
 
-//bugs 
-    //can proceed when choosing two players and having empty input
+    //algorithm for game pattern when choosing 1 or 2 player
+function playGame() {
+    if (onePlayer) {
+
+    }
+
+    else if (twoPlayers) {
+
+    }
+}
+
+    //restart button
+const restartBtn = document.querySelector(".reset-btn")
+const textInput = document.querySelectorAll("input[type='text']")
+
+function restart() {
+    modal.style.display = 'inherit';
+    playerName.innerHTML = ""
+    opponentName.innerHTML = ""
+    playerSymbolDiv.innerHTML = ""
+    opponentSymbolDiv.innerHTML = ""
+
+    textInput.forEach((element) => {
+        element.value = ""
+    })
+    squareArray.forEach((e) => {
+        e.innerHTML = ""
+    })
+
+}
+
+restartBtn.addEventListener("click", () => {
+    restart();
+})
     
+
+
+
+//features to implement
+    //playing against computer
+    //popUp when game over with button wanna play again?
+
+
