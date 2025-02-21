4e8ad80da80410cdde32154b25f11952df0ea50d
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-21 01:45:39 +0100

    added all of html and css

---
+++ Exercise-1/script.js
@@ -0,0 +1,9 @@
+// Tic tac toe game
+//features to implement
+    //choosing your symbol
+    //name input
+    //choosing how many rounds between 1/3/5
+        //scoreBoard when more than one round
+    //playing against computer
+    //reset Button
+    //popUp when game over with button wanna play again?
