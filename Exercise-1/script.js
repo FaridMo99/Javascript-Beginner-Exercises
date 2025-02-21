9c73522e59cba0b23c6f11333668bf8c4de70f59
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-21 04:04:21 +0100

    added some functions

--- Exercise-1/script.js
+++ Exercise-1/script.js
@@ -1,22 +1,63 @@
-//close pop up and add name and symbol
+//close pop up, show second pop up and add name and symbol
 const closeModal = document.querySelector("[data-close-modal]")
 const modal = document.querySelector("[data-modal]")
 const form = document.querySelector(".form")
+const playerAmountForm = document.querySelector(".playerAmount")
+const onePlayer = document.querySelector("#one")
+const twoPlayers = document.getElementById("two")
+const opponentNameDiv = document.querySelector(".opponentName")
+const buttonAmountOpponents = document.querySelector(".opp")
 
-function isFormValid() {
-    return form.checkValidity();
+
+function isFormValid(a) {
+    return a.checkValidity();
 }
 
 closeModal.addEventListener('click', (e) => {
     e.preventDefault()
-    if (isFormValid()) {
-        /*form.submit();  reloads the page so after learning to sve changes removing comments*/ 
-        
+
+    if (isFormValid(form)) {
         modal.style.display = 'none';
+        playerAmountForm.classList.toggle("secondHidden")
     } 
+
+    else {
+        alert("Missing Username")
+    }
 })
 
-// Tic tac toe game
+playerAmountForm.addEventListener("click", (e) => {
+
+
+    if(e.target.type === "radio"){
+
+    if(isFormValid(onePlayer) && !opponentNameDiv.classList.contains("thirdHidden")) {
+        opponentNameDiv.classList.add("thirdHidden")
+    }
+
+    else if (isFormValid(twoPlayers) && opponentNameDiv.classList.contains("thirdHidden")) {
+        opponentNameDiv.classList.remove("thirdHidden")
+    }
+
+}
+})
+
+buttonAmountOpponents.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if(isFormValid(onePlayer)) {
+        playerAmountForm.classList.toggle("secondHidden")
+    }
+
+    else if(isFormValid(twoPlayers) && isFormValid()) {
+        playerAmountForm.classList.toggle("secondHidden")
+    }
+})
+
+
+
+
+
 //features to implement
     //choosing your symbol
     //name input
@@ -25,3 +66,4 @@ closeModal.addEventListener('click', (e) => {
     //playing against computer
     //reset Button
     //popUp when game over with button wanna play again?
+    //function for 2 players or against computer
