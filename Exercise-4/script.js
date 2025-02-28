1b1a15749736c162c79ef83bb2e5a4a2ae911622
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-28 03:42:56 +0100

    added symbol shuffle and grid size functions

--- Exercise-4/script.js
+++ Exercise-4/script.js
@@ -2,6 +2,7 @@ let modal = document.querySelector("header")
 let modalButton = document.querySelector(".enterGridSizeBtn")
 let rows30 = document.querySelectorAll(".rows30")
 let rows40 = document.querySelectorAll(".rows40")
+let memoryPicturesAll = document.querySelectorAll("img")
 
 //modal closing and gridSize 
 
@@ -10,13 +11,19 @@ modalButton.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.toggle("hidden");
 
-    if (gridSize.value === "30"){
+    if (gridSize.value === "20") {
+        cutAndDupedArray(10)
+    }
+
+    else if (gridSize.value === "30"){
         nodeListToArrayAndLoop(rows30, "remove")
+        cutAndDupedArray(5)
     }
 
     else if (gridSize.value === "40") {
         nodeListToArrayAndLoop(rows30, "remove")
         nodeListToArrayAndLoop(rows40, "remove")
+        cutAndDupedArray(0)
 
     }
 })
@@ -37,10 +44,41 @@ function nodeListToArrayAndLoop(list, loopAction){
     //flipping cards
         //first card stays flipped 
             // if second card matches then both cards removed and add one to points
-    //randomized symbols under cards => make function and add to eventlistener modalButton
-        //when small take 10 svgs, clone and place randomly inside cells, medium 15, large 20
 
     //if all cards right  add to score you won and firework animation
     //restart and change grid button
-        //restart keeps grid but deletes everythign else
+        //restart keeps grid but deletes everything else
         //change grid deletes everything
+
+let allCells = Array.from(document.querySelectorAll(".cells"))
+let svgs = Array.from(document.querySelectorAll("img"))
+
+
+allCells.forEach(element => {
+    element.addEventListener("click", (e) => {
+        e.target.style.visibility = "hidden"
+        e.target.style.pointerEvents = "none"
+        svgs.style.visibility = "visible"
+
+    })
+})
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));        
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function cutAndDupedArray(lengthToCut) {
+    let randomizedArray = shuffleArray(svgs);
+    randomizedArray.splice(0, lengthToCut)
+    randomizedArray.forEach(item => {
+        dupedArray.push(item, item);
+        return dupedArray
+    })
+}
+
+function placeSvg() {
+
+}
