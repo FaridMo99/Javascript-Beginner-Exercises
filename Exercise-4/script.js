32e53789b2245259f0e9d2eef94314feda40f0ff
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-28 18:23:27 +0100

    added shuffling and placing symbols functions

--- Exercise-4/script.js
+++ Exercise-4/script.js
@@ -1,30 +1,42 @@
 let modal = document.querySelector("header")
 let modalButton = document.querySelector(".enterGridSizeBtn")
+
 let rows30 = document.querySelectorAll(".rows30")
 let rows40 = document.querySelectorAll(".rows40")
-let memoryPicturesAll = document.querySelectorAll("img")
+
+let allCells = document.querySelectorAll(".cells")
+let svgs = document.querySelectorAll("img")
+
+let scoreboard = document.querySelector(".containerScore")
 
 //modal closing and gridSize 
 
 modalButton.addEventListener("click", (e) => {
-    let gridSize = document.querySelector("input[type='radio']:checked")
+    let gridSize = document.querySelector("input[type='radio']:checked");
+   
     e.preventDefault();
     modal.classList.toggle("hidden");
 
     if (gridSize.value === "20") {
-        cutAndDupedArray(10)
+        nodeListToArrayAndLoop(allCells, "addEventListener")
+        placeSvg(10);
+        updateScore(0, 10);
     }
 
     else if (gridSize.value === "30"){
-        nodeListToArrayAndLoop(rows30, "remove")
-        cutAndDupedArray(5)
+        nodeListToArrayAndLoop(allCells, "addEventListener")
+        nodeListToArrayAndLoop(rows30, "remove");
+        placeSvg(5);
+        updateScore(0, 15);
+
     }
 
     else if (gridSize.value === "40") {
-        nodeListToArrayAndLoop(rows30, "remove")
-        nodeListToArrayAndLoop(rows40, "remove")
-        cutAndDupedArray(0)
-
+        nodeListToArrayAndLoop(allCells, "addEventListener")
+        nodeListToArrayAndLoop(rows30, "remove");
+        nodeListToArrayAndLoop(rows40, "remove");
+        placeSvg(0);
+        updateScore(0, 20);
     }
 })
 
@@ -36,7 +48,23 @@ function nodeListToArrayAndLoop(list, loopAction){
         element.classList[loopAction]('hidden');
         }
 
+        else if (loopAction === "addEventListener") {
+            element.addEventListener("click", (e) => {
+
+            e.target.style.visibility = "hidden"
+            e.target.style.pointerEvents = "none"   
+            })
+        }
+
     })
+}
+
+function updateScore(score, combinations) {
+    scoreboard.innerHTML = `${score}/${combinations}`
+
+    if(score === combinations) {
+        scoreboard.innerHTML += " You Won!"
+    }
 }
 
 //features 
@@ -50,35 +78,42 @@ function nodeListToArrayAndLoop(list, loopAction){
         //restart keeps grid but deletes everything else
         //change grid deletes everything
 
-let allCells = Array.from(document.querySelectorAll(".cells"))
-let svgs = Array.from(document.querySelectorAll("img"))
 
+function shuffleArray(arrayy) {
+    let array = Array.from(arrayy)
 
-allCells.forEach(element => {
-    element.addEventListener("click", (e) => {
-        e.target.style.visibility = "hidden"
-        e.target.style.pointerEvents = "none"
-        svgs.style.visibility = "visible"
-
-    })
-})
-
-function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));        
         [array[i], array[j]] = [array[j], array[i]];
     }
+
+    return array
 }
 
 function cutAndDupedArray(lengthToCut) {
     let randomizedArray = shuffleArray(svgs);
-    randomizedArray.splice(0, lengthToCut)
+    let dupedArray = []
+
+    randomizedArray.splice(0, lengthToCut);
+
     randomizedArray.forEach(item => {
         dupedArray.push(item, item);
-        return dupedArray
-    })
+    });
+
+    return shuffleArray(dupedArray);
 }
 
-function placeSvg() {
+function placeSvg(lengthToCut) {
+    let shuffledDupedArray = cutAndDupedArray(lengthToCut);
+    let index = 0
+
+    allCells.forEach((cell) => {
+        let svg = shuffledDupedArray[index];
+        cell.innerHTML = svg.outerHTML
+        console.log(svg)
+        index++
+    });
 
 }
+
+
